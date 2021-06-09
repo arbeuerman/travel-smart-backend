c06ffcb46b6638e2535e8fba6bd66adcc79e80fa
Alexandra <arbeuerman@gmail.com>
2021-06-09 18:38:44 -0500

    activities table and dat

--- Gemfile
+++ Gemfile
@@ -23,7 +23,7 @@ gem 'puma', '~> 5.0'
 gem 'bootsnap', '>= 1.4.4', require: false
 
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
-# gem 'rack-cors'
+gem 'rack-cors'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
@@ -38,3 +38,12 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+#travel api access
+gem 'amadeus'
+
+#faker gem to generate data for initial seeding
+gem 'faker'
+
+gem 'activerecord-reset-pk-sequence'
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Activity.destroy_all
+Activity.reset_pk_sequence
+
+
+10.times do 
+    Activity.create(
+        name: Faker::Mountain.unique.name,
+        location: Faker::Nation.unique.capital_city,
+        imageUrl: Faker::Avatar.unique.image,
+        category: ["hiking", "food", "drinks", "museums", "culture"].sample
+        )
+end
+
+##################################
+###### Trying to Access API ######
+##################################
+# amadeus = Amadeus::Client.new({
+#   client_id: '8vcUEDJFv2UhYzwB5KmgTlRyC76jMD5H',
+#   client_secret: 'BAV4rSS5k1XoQ1dq'
+# })
+
+# def locations
+#     Amadeus::Namespaces::ReferenceData::Locations.new(client)
+# end
+
+# response = amadeus.reference_data.locations.point_of_interest('9CB40CB5D0').get()
+# name = response.data['name']
+# # pointsOfInterest = amadeus.reference_data.locations.points_of_interest.get(latitude: 41.397158, longitude: 2.160873)
+# pointsOfInterest = amadeus.reference_data.locations.points_of_interest.get(latitude: 35.6897, longitude: 139.6922)
+# pOIs = pointsOfInterest.data
+# # category = response.data['category']
+# # tags = response.data['tags']
+
+# locations = amadeus.reference_data.locations
+
+# # locations = amadeus.reference_data.locations.get()
+# # allPOIs = amadeus.reference_data.locations.points_of_interest
+
+# byebug
+# puts "done"
+# # byebug
