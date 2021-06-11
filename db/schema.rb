606d0b39097e9abe01d8cc3a5e42a2a2494ddcd8
Alexandra <arbeuerman@gmail.com>
2021-06-11 13:02:05 -0500

    adding users and favorites tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_08_225609) do
+ActiveRecord::Schema.define(version: 2021_06_11_175912) do
 
   create_table "activities", force: :cascade do |t|
     t.string "name"
@@ -21,4 +21,24 @@ ActiveRecord::Schema.define(version: 2021_06_08_225609) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "activity_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["activity_id"], name: "index_favorites_on_activity_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.string "imageUrl"
+    t.string "bio"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "favorites", "activities"
+  add_foreign_key "favorites", "users"
 end
